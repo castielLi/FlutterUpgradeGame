7fff57b53f577ab00c0f1ee39b4d07b1b9f0b879
gqdxcz <32891982+gqdxcz@users.noreply.github.com>
2020-05-20 14:40:10 +0800

    changes
    changes

---
+++ lib/Src/model/MainPalace.dart
@@ -0,0 +1,17 @@
+// 主建筑
+class MainPalace {
+  // 等级
+  int level;
+  // 升到本等级需要的木头
+  int neededWood;
+  // 升到本等级需要的石头
+  int neededStone;
+  // 本等级每小时产生的币
+  int coinPerHour;
+
+  MainPalace({this.level, this.neededWood, this.neededStone, this.coinPerHour});
+
+
+
+
+}
--- lib/Src/pages/farm/farm.dart
+++ lib/Src/pages/farm/farm.dart
@@ -10,8 +10,6 @@ class FarmDetail extends StatefulWidget {
 
 class _FarmDetailState extends State<FarmDetail> {
 
-
-///
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -20,19 +18,83 @@ class _FarmDetailState extends State<FarmDetail> {
 
   @override
   Widget build(BuildContext context) {
+//    var textStyle32 = TextStyle(fontSize: 32.0, color: Colors.white,
+//        decoration: TextDecoration.none);
 
     return new Container(
       child: new Container(
-        margin: EdgeInsets.fromLTRB(ScreenUtil().setWidth(100), ScreenUtil().setHeight(300), ScreenUtil().setWidth(100), ScreenUtil().setHeight(120)),
-        color: Colors.black,
+        margin: EdgeInsets.fromLTRB(
+            ScreenUtil().setWidth(150),
+            ScreenUtil().setHeight(400),
+            ScreenUtil().setWidth(150),
+            ScreenUtil().setHeight(180)),
+        color: Colors.transparent,
+        child: ListView(
+          itemExtent: 60,// list高度
+          children: <Widget>[
+            Text('LV 12 > LV 13',textAlign:TextAlign.left,style: TextStyle(fontSize: 32.0, color: Colors.white,
+                decoration: TextDecoration.none),),
+            Text('升级所需资料',style: TextStyle(fontSize: 32.0, color: Colors.white,
+                decoration: TextDecoration.none),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new Image(image: new AssetImage('resource/images/wood.png'),
+                    height:30),
+                Text('2910 ',style: TextStyle(fontSize: 30.0, color: Colors.white,
+                  decoration: TextDecoration.none,),),
+                new Image(image: new AssetImage('resource/images/stone.png'),
+                    height:30),
+                Text('2910',style: TextStyle(fontSize: 30.0, color: Colors.white,
+                  decoration: TextDecoration.none,)),
+              ],
+            ),
+            Text('升级后产出:291T币一小时',style:TextStyle(fontSize: 23.0, color: Colors.white,
+              decoration: TextDecoration.none,),),
+            RaisedButton(onPressed: (){},
+              child: Text('升级'),
+
+            ),
+          ],
+        ),
+        /*
         child:Center(
           child:
-          new Image(image: new AssetImage('resource/images/welcome.png'),
-            fit: BoxFit.fill,
-            height: 100 ,
-            width: 100,
-          ),
+//          new Image(image: new AssetImage('resource/images/welcome.png'),
+//            fit: BoxFit.fill,
+//            height: 100 ,
+//            width: 100,
+//          ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            Text('LV 12 > LV 13',textAlign:TextAlign.left,style: TextStyle(fontSize: 32.0, color: Colors.white,
+                decoration: TextDecoration.none,backgroundColor: Colors.blue),),
+            Text('升级所需资料',style: TextStyle(fontSize: 32.0, color: Colors.white,
+              decoration: TextDecoration.none,backgroundColor: Colors.red),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Image(image: new AssetImage('resource/images/wood.png'),
+                height:30),
+                Text('2910',style: TextStyle(fontSize: 30.0, color: Colors.white,
+                  decoration: TextDecoration.none,),),
+                new Image(image: new AssetImage('resource/images/stone.png'),
+                height:30),
+                Text('2910',style: TextStyle(fontSize: 30.0, color: Colors.white,
+                  decoration: TextDecoration.none,)),
+              ],
+            ),
+            Text('升级后产出:291T币一小时',style:TextStyle(fontSize: 23.0, color: Colors.white,
+              decoration: TextDecoration.none,),),
+            RaisedButton(onPressed: (){},
+              child: Text('升级'),
+            ),
+          ],
         ),
+        ),
+        */
       ),
     );
   }
