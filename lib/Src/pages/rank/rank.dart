31c70bed78cc93bf2db10f654588c567ee4c9393
gqdxcz <32891982+gqdxcz@users.noreply.github.com>
2020-05-22 15:00:39 +0800

    0522
    0522

--- lib/Src/pages/rank/rank.dart
+++ lib/Src/pages/rank/rank.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:upgradegame/Common/widget/adIconRow/adIconRow.dart';
 import 'package:upgradegame/Common/widget/imageButton/imageButton.dart';
 
 
@@ -30,31 +31,51 @@ class _RankDetailState extends State<RankDetail> {
             ScreenUtil().setHeight(400),  // 上
             ScreenUtil().setWidth(120),   // 右
             ScreenUtil().setHeight(220)), // 下
-        color: Colors.blue,
-//        child:Text('T币 提现',style: textStyle,),
-
-//        ListView.builder(
-//            itemCount: 10,
-////                itemExtent: 30,
-//            itemBuilder: (BuildContext context, int index){
-//              return ListTile(title: Text("$index"));
-//            }
-//        ),
-
-
-//        new Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            Text('T币 提现'),
-//            ListView.builder(
-//                itemCount: 10,
-////                itemExtent: 30,
-//                itemBuilder: (BuildContext context, int index){
-//                  return ListTile(title: Text("$index"));
-//                }
+//        color: Colors.blue,
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  GestureDetector(
+                    child:
+//                      Stack(
+//                        children: <Widget>[
+//                          Image(image: new AssetImage('resource/images/rank/yellowButton.png'),
+////                            width: MediaQuery.of(context).size.width,
+////                            height: MediaQuery.of(context).size.height,
+////                            fit: BoxFit.fill,
+//                          ),
+////                          Row(
+////                            children: <Widget>[
+////                              Image(image: new AssetImage('resource/images/rank/coin.png'),height: 40,),
+//////                              Text('T币'),
+////                            ],
+////                          ),
+//                        ],
+//                      ),
+                    new Image(image: new AssetImage('resource/images/rank/yellowButton.png'),height: 50,),
+                    onTap: (){print('点击T币');},
+                  ),
+//                  GestureDetector(
+//                    child: new Image(image: new AssetImage('resource/images/stone.png'),height: 50,),
+//                    onTap: (){print('点击提现');},
+//                  ),
+                ],
+              ),
+            ),
+//            Container(
+//              child:ListView.builder(
+//                  itemBuilder: null
+//              ),
 //            ),
-//          ],
-//        ),
+
+          ],
+        ),
+
+
       ),
     );
   }
