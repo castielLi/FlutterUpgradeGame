7237d59204230af56324caf41ada3941bd67d714
gqdxcz <32891982+gqdxcz@users.noreply.github.com>
2020-05-29 17:15:15 +0800

    0529
    0529

--- lib/Src/pages/userInfo/account/account.dart
+++ lib/Src/pages/userInfo/account/account.dart
@@ -5,37 +5,75 @@ class AccountDetail extends StatefulWidget {
   @override
   VoidCallback HUD;
   VoidCallback viewCallback;
-  AccountDetail({Key key,this.HUD,this.viewCallback}):super(key:key);
+
+  AccountDetail({Key key, this.HUD, this.viewCallback}) : super(key: key);
+
   _AccountDetailState createState() => new _AccountDetailState();
 }
 
 class _AccountDetailState extends State<AccountDetail> {
-
-
-
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return new Container(
       margin: EdgeInsets.fromLTRB(
-          ScreenUtil().setWidth(0),   // 左
-          ScreenUtil().setHeight(120),  // 上
-          ScreenUtil().setWidth(0),   // 右
-          ScreenUtil().setHeight(150),
+        ScreenUtil().setWidth(0), // 左
+        ScreenUtil().setHeight(120), // 上
+        ScreenUtil().setWidth(0), // 右
+        ScreenUtil().setHeight(150),
       ),
-      color:Colors.lightBlue,
-      child:
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          TextField(),
-//          TextField(),
-        ],
+      color: Colors.lightBlue,
+      child: Scaffold(
+//        body: Flex(
+//          direction: Axis.vertical,
+//          children: <Widget>[
+//            TextField(
+//              autofocus: true,
+//              decoration: InputDecoration(
+//                  labelText: "原密码", prefixIcon: Icon(Icons.lock)),
+//            ),
+//            TextField(
+//              decoration: InputDecoration(
+//                  labelText: "新密码", prefixIcon: Icon(Icons.lock)),
+//              obscureText: true,
+//            ),
+//            TextField(
+//              decoration: InputDecoration(
+//                  labelText: "重复密码", prefixIcon: Icon(Icons.lock)),
+////              obscureText: true,
+//            ),
+//          ],
+//        ),
+//              Column(
+//                mainAxisAlignment: MainAxisAlignment.start,
+//                children: <Widget>[
+//                  TextField(
+//                    autofocus: true,
+//                    decoration: InputDecoration(
+//                        labelText: "原密码",
+//                        prefixIcon: Icon(Icons.lock)
+//                    ),
+//                  ),
+//                  TextField(
+//                    decoration: InputDecoration(
+//                        labelText: "新密码",
+//                        prefixIcon: Icon(Icons.lock)
+//                    ),
+//                    obscureText: true,
+//                  ),
+//                  TextField(
+//                    decoration: InputDecoration(
+//                        labelText: "重复密码",
+//                        prefixIcon: Icon(Icons.lock)
+//                    ),
+//                    obscureText: true,
+//                  ),
+//                ],
+//              )
       ),
 //      Stack(
 //        children: <Widget>[
