43351d2a1a4efe757f250aafb397ac55304f691f
gqdxcz <32891982+gqdxcz@users.noreply.github.com>
2020-05-29 15:50:25 +0800

    0529
    0529

--- lib/Src/pages/setting/private/private.dart
+++ lib/Src/pages/setting/private/private.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:upgradegame/Common/app/config.dart';
-import 'package:upgradegame/Src/route/application.dart';
-import 'package:upgradegame/Src/route/upgradegame_route.dart';
 import 'package:upgradegame/Common/widget/imageButton/imageButton.dart';
 
 class PrivateDetail extends StatefulWidget {
   @override
   VoidCallback HUD;
   VoidCallback viewCallback;
-  PrivateDetail({Key key,this.HUD,this.viewCallback}):super(key:key);
+
+  PrivateDetail({Key key, this.HUD, this.viewCallback}) : super(key: key);
+
   _PrivateDetailState createState() => new _PrivateDetailState();
 }
 
 class _PrivateDetailState extends State<PrivateDetail> {
-
-
+  String privacyContext =
+      '124321542153214321425434321432143243215432542314323333124321542153214321425434321432143243215432542314323333124321542153214321425434321432143243215432542314323333124321542153214321425434321432143243215432542314323333';
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-
   }
 
   @override
@@ -29,13 +27,34 @@ class _PrivateDetailState extends State<PrivateDetail> {
       child: Stack(
         children: <Widget>[
           new Container(
-            child:Center(
-              child:
-              new ImageButton(height:ScreenUtil().setHeight(200),width: ScreenUtil().setWidth(400),buttonName: "升 级",imageUrl: "resource/images/upgradeButton.png",callback: (){
-                print('点击升级');
+            margin: EdgeInsets.fromLTRB(
+                ScreenUtil().setWidth(0), // 左
+                ScreenUtil().setHeight(120), // 上
+                ScreenUtil().setWidth(0), // 右
+                ScreenUtil().setHeight(120)),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: ScreenUtil().setHeight(820),
+                  child: ListView(
+                    children: <Widget>[
+                      Text(this.privacyContext),
+                    ],
+                  ),
+                ),
+                new ImageButton(
+                  height: ScreenUtil().setHeight(200),
+                  width: ScreenUtil().setWidth(400),
+                  buttonName: "返回",
+                  imageUrl: "resource/images/upgradeButton.png",
+                  callback: () {
+                    print('点击升级');
 //                this.widget.HUD();
-                this.widget.viewCallback();
-              },),
+                    this.widget.viewCallback();
+                  },
+                ),
+              ],
             ),
           ),
         ],
