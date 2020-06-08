941916c1ff9ae8504b79f205cada4f69e4abd8d4
castiel <252182398@qq.com>
2020-06-08 15:06:23 +0800

    Revert "Merge branch 'master' into lzj"
    This reverts commit 1a46e90dc78b1034f7760c7906b266c5a7edae07, reversing
    changes made to be85435f93a4e13e7b87a72998bce2b43f33ec0c.

---
+++ android/app/src/main/java/com/example/upgradegame/adview/AdSpreadActivity.java
@@ -0,0 +1,114 @@
+package com.example.upgradegame.adview;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.example.upgradegame.Constant;
+import com.example.upgradegame.R;
+import com.kuaiyou.open.AdManager;
+import com.kuaiyou.open.InitSDKManager;
+import com.kuaiyou.open.SpreadManager;
+import com.kuaiyou.open.interfaces.AdViewSpreadListener;
+
+import java.util.List;
+
+/**
+ * 开屏
+ */
+public class AdSpreadActivity extends Activity implements AdViewSpreadListener {
+    private SpreadManager spreadManager = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.spread_layout);
+        requestSpreadAd(getIntent().getStringExtra("posId"));
+    }
+
+    private void requestSpreadAd(String posId) {
+        if(TextUtils.isEmpty(posId)){
+            posId = Constant.SPREADPOSID;
+        }
+        spreadManager = AdManager.createSpreadAd();
+        spreadManager.loadSpreadAd(this, Constant.APPID, posId,
+                (RelativeLayout) findViewById(R.id.spreadlayout));
+        spreadManager.setBackgroundColor(Color.WHITE);
+        spreadManager.setSpreadNotifyType(AdManager.NOTIFY_COUNTER_NUM);
+        spreadManager.setSpreadListener(this);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+            return false;
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onAdClicked() {
+        Log.i("AdViewDemo", "onAdClicked");
+    }
+
+    @Override
+    public void onAdClosed() {
+        Log.i("AdViewDemo", "onAdClosedAd");
+        jump();
+    }
+
+    @Override
+    public void onAdClosedByUser() {
+        Log.i("AdViewDemo", "onAdClosedByUser");
+        jump();
+    }
+
+    @Override
+    public void onRenderSuccess() {
+
+    }
+
+    @Override
+    public void onAdDisplayed() {
+        Log.i("AdViewDemo", "onAdDisplayed");
+    }
+
+    @Override
+    public void onAdFailedReceived(String arg1) {
+        Log.i("AdViewDemo", "onAdRecieveFailed");
+        jump();
+    }
+
+    @Override
+    public void onAdReceived() {
+        Log.i("AdViewDemo", "onAdRecieved");
+    }
+
+    @Override
+    public void onAdSpreadPrepareClosed() {
+        Log.i("AdViewDemo", "onAdSpreadPrepareClosed");
+        jump();
+    }
+
+    private void jump() {
+        this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != spreadManager)
+            spreadManager.destroy();
+    }
+}
